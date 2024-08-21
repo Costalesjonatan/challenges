d5f431140ebf1711a7f440d7d414e2a6b0e2ce0b
Jonatan <costalesjonatan@gmail.com>
2024-08-21 11:10:29 -0300

    Lombok dependency added.

--- src/main/java/org/app/Main.java
+++ src/main/java/org/app/Main.java
@@ -1,10 +1,13 @@
 package org.app;
 
+import lombok.extern.java.Log;
+
+@Log
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        log.info("Hello world!");
         for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+            log.info("i = " + i);
         }
     }
 }
